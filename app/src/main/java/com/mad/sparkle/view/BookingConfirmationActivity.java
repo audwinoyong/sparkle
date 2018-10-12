7ed29c7df178edc2faccdaa401c2bdf5ba2009da
Audwin <audwinoyong@live.com>
2018-10-12 14:07:37 +1100

    Refactor booking confirmation activity

--- app/src/main/java/com/mad/sparkle/view/BookingConfirmationActivity.java
+++ app/src/main/java/com/mad/sparkle/view/BookingConfirmationActivity.java
@@ -1,15 +1,30 @@
 package com.mad.sparkle.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.mad.sparkle.R;
 
 public class BookingConfirmationActivity extends AppCompatActivity {
 
+    private Button mHomeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_confirmation);
+
+        mHomeBtn = (Button) findViewById(R.id.activity_booking_confirmation_home_button);
+        mHomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homeIntent = new Intent(getApplicationContext(), NavigationActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });
     }
 }
